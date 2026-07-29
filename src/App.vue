8f84d9c0bbdb2f8717e7d422f37b5ee0bbb7e849
gajelou <125290990+gajelou@users.noreply.github.com>
2026-07-29 10:12:45 -0300

    Update API_URL to use HTTPS and new endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 
-const API_URL = "http://187.127.47.66:3002";
+const API_URL = "https://187.127.47.66/comercial";
 
 type Produto = {
   codigo?: string;
